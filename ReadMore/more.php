e51ad90a460ab65a7bd31aac98d0f02ff75f6e3e
itsumoonazicode <dyingkouki@gmail.com>
2017-05-15 00:59:20 +0900

    エラー"Trying to get property of non-object"が発生

--- ReadMore/more.php
+++ ReadMore/more.php
@@ -2,12 +2,31 @@
 require_once('codebird.php');
 require_once('config.php');
 
-Codebird::setConsumerKey('CONSUMER_KEY', 'CONSUMER_SECRET'); // static, see README
+\Codebird\Codebird::setConsumerKey('CONSUMER_KEY', 'CONSUMER_SECRET');
 
-$cb = Codebird::getInstance();
+$cb = \Codebird\Codebird::getInstance();
 
 $cb->setToken('ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET');
 
-$tweets = $cd->statuses_userTimeline();
+$params = array(
+    'screen_name' => 'dawaruka',
+    'include_rts' => true
+);
+
+$tweets =  (array) $cb->statuses_userTimeline($params);
+
+
+// remove rate
+array_pop($tweets);
+// remove httpstatus
+array_pop($tweets);
+
+foreach($tweets as $tweet) {
+    if (isset($tweet) == true) {
+        echo '<li id="tweet_' .$tweet->id_str . ' ">' .$tweet->text . '</li>';
+    }else{
+        echo '<li id="tweet_' .$tweet->id_str . ' ">' .$tweet->text . '</li>';
+    }
+}
 
 ?>
